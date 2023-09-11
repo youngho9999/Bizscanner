987c603ef7750b28fc3fb7a468feca1b23ad941f
전영호 <aplblypt@gmail.com>
2023-09-11 16:05:46 +0900

    chore: Init dev branch

--- backend/bizscanner/src/main/java/store/bizscanner/controller/ReportController.java
+++ backend/bizscanner/src/main/java/store/bizscanner/controller/ReportController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 @RequestMapping("/report")
 @RequiredArgsConstructor
 public class ReportController {
