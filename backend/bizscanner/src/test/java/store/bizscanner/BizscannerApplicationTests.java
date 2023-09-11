f1f2519cfd39924f702911ce113d2e959061d0b1
전영호 <aplblypt@gmail.com>
2023-09-11 16:01:05 +0900

    chore: Init Spring Boot project

---
+++ backend/bizscanner/src/test/java/store/bizscanner/BizscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package store.bizscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizscannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
