42dcd4ff004112ba8389f8a14fde4a9238a86f37
이현구 <wancern@naver.com>
2023-09-13 15:04:04 +0900

    feat: carea find 메소드 생성

--- backend/bizscanner/src/main/java/store/bizscanner/repository/CareaRepository.java
+++ backend/bizscanner/src/main/java/store/bizscanner/repository/CareaRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import store.bizscanner.entity.Carea;
 
+import java.util.Optional;
+
 @Repository
 public interface CareaRepository extends JpaRepository<Carea, Long> {
+    Optional<Carea> findByCareaCode(String careaCode);
 }
--- backend/bizscanner/src/main/java/store/bizscanner/service/CareaService.java
+++ backend/bizscanner/src/main/java/store/bizscanner/service/CareaService.java
@@ -3,6 +3,9 @@ package store.bizscanner.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import store.bizscanner.entity.Carea;
+import store.bizscanner.global.exception.CustomException;
+import store.bizscanner.global.exception.ErrorCode;
 import store.bizscanner.repository.CareaRepository;
 
 @Service
@@ -10,4 +13,8 @@ import store.bizscanner.repository.CareaRepository;
 @RequiredArgsConstructor
 public class CareaService {
     private final CareaRepository careaRepository;
+
+    public Carea findByCareaCode(String careaCode){
+        return careaRepository.findByCareaCode(careaCode).orElseThrow(() -> new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND));
+    }
 }
