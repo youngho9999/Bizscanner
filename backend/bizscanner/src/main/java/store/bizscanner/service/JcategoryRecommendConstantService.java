37eb26cc97771ad2d1aab28a0f75070753d89eff
이현구 <wancern@naver.com>
2023-09-18 16:49:03 +0900

    feat: 업종추천 시 폐업률 필터링 추가

--- backend/bizscanner/src/main/java/store/bizscanner/service/JcategoryRecommendConstantService.java
+++ backend/bizscanner/src/main/java/store/bizscanner/service/JcategoryRecommendConstantService.java
@@ -28,15 +28,18 @@ public class JcategoryRecommendConstantService {
      */
     public JcategoryRecommendResponse getJcategoryRecommend(String careaCode){
         List<JcategoryRecommendMapping> recommendList = salesRepository.getJcategoryRecommend(careaCode);
-        if(recommendList.isEmpty()){
-            throw new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND);
-        }
 
-        return new JcategoryRecommendResponse(
-                recommendList.get(0),
-                jcategoryRecommendConstantRepository.findByCareaCode(careaCode)
-                        .orElseThrow(()->new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND)),
-                storeRepository.findTopByCareaCodeAndJcategoryCodeOrderByYearCodeDescQuarterCodeDesc(careaCode, recommendList.get(0).getJcategoryCode())
-                        .orElseThrow(() -> new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND)).getCloseRate());
+        for(JcategoryRecommendMapping jcategoryRecommendMapping : recommendList){
+            Integer closeRate = storeRepository.findTopByCareaCodeAndJcategoryCodeOrderByYearCodeDescQuarterCodeDesc(careaCode, jcategoryRecommendMapping.getJcategoryCode())
+                    .orElseThrow(() -> new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND)).getCloseRate();
+
+            if(closeRate < 15){
+                return new JcategoryRecommendResponse(jcategoryRecommendMapping,
+                        jcategoryRecommendConstantRepository.findByCareaCode(careaCode)
+                                .orElseThrow(()->new CustomException(ErrorCode.REPORT_RESOURCE_NOT_FOUND)),
+                        closeRate);
+            }
+        }
+        throw new CustomException(ErrorCode.RECOMMEND_JCATEGORY_NOT_FOUND);
     }
 }
