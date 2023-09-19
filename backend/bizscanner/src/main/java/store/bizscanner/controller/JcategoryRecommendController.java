3c59a8e7254d4fd27be158fdb2a867d4b387f8d4
이현구 <wancern@naver.com>
2023-09-19 09:55:57 +0900

    feat: 행정동 내 상권 리스트 호출 API 구현

--- backend/bizscanner/src/main/java/store/bizscanner/controller/JcategoryRecommendController.java
+++ backend/bizscanner/src/main/java/store/bizscanner/controller/JcategoryRecommendController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import store.bizscanner.dto.response.jcategoryrecommend.DongResponse;
 import store.bizscanner.dto.response.jcategoryrecommend.JcategoryRecommendResponse;
+import store.bizscanner.service.CareaService;
 import store.bizscanner.service.JcategoryRecommendConstantService;
 
 @RestController
@@ -13,9 +15,15 @@ import store.bizscanner.service.JcategoryRecommendConstantService;
 @RequiredArgsConstructor
 public class JcategoryRecommendController {
     private final JcategoryRecommendConstantService jcategoryRecommendConstantService;
+    private final CareaService careaService;
 
     @GetMapping("/{careaCode}")
     public ResponseEntity<JcategoryRecommendResponse> getJcategoryRecommend(@PathVariable String careaCode){
         return new ResponseEntity<>(jcategoryRecommendConstantService.getJcategoryRecommend(careaCode), HttpStatus.OK);
+    }
+
+    @GetMapping("/dong/{dong}")
+    public ResponseEntity<DongResponse> findCareaByDong(@PathVariable String dong) {
+        return new ResponseEntity<>(careaService.findByDong(dong), HttpStatus.OK);
     }
 }
