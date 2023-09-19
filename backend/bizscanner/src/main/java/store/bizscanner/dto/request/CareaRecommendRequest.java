3c7c062d9ea43534972d7d11f633f976c7473b2b
전영호 <aplblypt@gmail.com>
2023-09-19 17:33:24 +0900

    feat: responseDTO 세터 제거

--- backend/bizscanner/src/main/java/store/bizscanner/dto/request/CareaRecommendRequest.java
+++ backend/bizscanner/src/main/java/store/bizscanner/dto/request/CareaRecommendRequest.java
@@ -1,9 +1,8 @@
 package store.bizscanner.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
+@Getter
 public class CareaRecommendRequest {
     private String jcategoryCode;
     private Long firstInvestmentAmount;
