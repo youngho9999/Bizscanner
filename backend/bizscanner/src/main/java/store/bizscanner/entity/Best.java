3edb9e18d9b81a6c279871cb37ce0b577a0390ce
이현구 <wancern@naver.com>
2023-09-19 16:53:13 +0900

    refactor: Best 클래스 분리

---
+++ backend/bizscanner/src/main/java/store/bizscanner/entity/Best.java
@@ -0,0 +1,24 @@
+package store.bizscanner.entity;
+
+import lombok.Getter;
+
+/**
+ * Best 항목을 찾기 위한 Class
+ * 여러 Enum Type에 대응하기 위해 Object로 선언
+ * value를 기준으로 정렬하는 규칙 생성
+ */
+@Getter
+public class Best implements Comparable<Best> {
+    private final Long value;
+    private final Object object;
+
+    public Best (Long value, Object object) {
+        this.value = value;
+        this.object = object;
+    }
+
+    @Override
+    public int compareTo(Best o) {
+        return Long.compare(this.value, o.value);
+    }
+}
