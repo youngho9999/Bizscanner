80f39aa3d7daab92f76a49e1aa0c3d39ba3b51c6
전영호 <aplblypt@gmail.com>
2023-09-20 11:38:41 +0900

    feat: 상권추천 api 컨트롤러에 crossorigin 추가

--- backend/bizscanner/src/main/java/store/bizscanner/controller/CareaRecommendController.java
+++ backend/bizscanner/src/main/java/store/bizscanner/controller/CareaRecommendController.java
@@ -3,6 +3,7 @@ package store.bizscanner.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import store.bizscanner.service.CareaRecommendService;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class CareaRecommendController {
     private final CareaRecommendService careaRecommendService;
 
