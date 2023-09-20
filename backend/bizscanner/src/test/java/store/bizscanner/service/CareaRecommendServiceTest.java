c34a21d530dcb73bb26e6b85e603d3ce05137399
전영호 <aplblypt@gmail.com>
2023-09-20 11:20:41 +0900

    test: 업종별 상권 추천 확인 test 코드 작성

--- backend/bizscanner/src/test/java/store/bizscanner/service/CareaRecommendServiceTest.java
+++ backend/bizscanner/src/test/java/store/bizscanner/service/CareaRecommendServiceTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import store.bizscanner.repository.SalesRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,8 +17,15 @@ class CareaRecommendServiceTest {
 
     @Autowired
     private CareaRecommendService careaRecommendService;
+    @Autowired
+    private SalesRepository salesRepository;
 
     @Test
     public void checkFind() {
+        List<String> distinctJcategory = salesRepository.findDistinctJcategory();
+
+        List<String> collect = distinctJcategory.stream().map(x -> careaRecommendService.findCareaRecommend(x, 100000000000L, 60L).getCareaCode())
+                .collect(Collectors.toList());
+        collect.forEach(System.out::println);
     }
 }
