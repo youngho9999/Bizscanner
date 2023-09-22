4d8899b30aef63bae29be45f4606035c06a9f3e1
seo-jio <lio8625@naver.com>
2023-09-22 16:17:50 +0900

    chore: member 클래스 추가

---
+++ backend/bizscanner/src/main/java/store/bizscanner/entity/Member.java
@@ -0,0 +1,27 @@
+package store.bizscanner.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member extends BaseTime {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String provider;
+}
