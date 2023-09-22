fca4c3f59d604caa6db1c0150e4d7ae4a97211a0
seo-jio <lio8625@naver.com>
2023-09-22 16:18:14 +0900

    chore: BaseTime 추가

--- backend/bizscanner/src/main/java/store/bizscanner/BizscannerApplication.java
+++ backend/bizscanner/src/main/java/store/bizscanner/BizscannerApplication.java
@@ -2,7 +2,9 @@ package store.bizscanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BizscannerApplication {
 
