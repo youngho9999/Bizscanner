16ea2ff1caa5ee5e58beef87e3b918bf4f9b147f
이국신 <nw4611@naver.com>
2023-09-22 10:57:29 +0900

    feat: 검색 컨트롤러 구현

--- frontend/src/app/scan/page.jsx
+++ frontend/src/app/scan/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import data from '../../../public/상권특구.geojson';
 import Map from './Map';
+import Controller from './Controller';
 
 function page() {
   return (
-    <div className="flex grow">
+    <div className="relative flex grow">
+      <Controller />
       <Map commerialDistricts={data} />
     </div>
   );
