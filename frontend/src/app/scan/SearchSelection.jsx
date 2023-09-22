0fe68f9cf9880fc1a9a397703c0090c4410ba791
이국신 <nw4611@naver.com>
2023-09-22 17:10:49 +0900

    feat: 컨트롤러 타이틀 컴포넌트 분리

--- frontend/src/app/scan/SearchSelection.jsx
+++ frontend/src/app/scan/SearchSelection.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import SelectButton from './SearchSelectButton';
 import LocationIcon from '@/assets/icons/location_on.svg';
 import StoreIcon from '@/assets/icons/local_convenience_store.svg';
+import ControllerTitle from './ControllerTitle';
 
 function SearchSelection() {
   return (
     <>
-      <div className="mt-10 mb-8 text-2xl text-center">무엇으로 검색하시겠어요?</div>
+      <ControllerTitle title={"무엇으로 검색하시겠어요?"} />
       <div className="flex gap-10 mb-10">
         <SelectButton text="상권" Icon={LocationIcon} />
         <SelectButton text="업종" Icon={StoreIcon} />
