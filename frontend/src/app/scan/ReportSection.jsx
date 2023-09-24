57452e7e330e1b565a7768a803cf518fc1dc7cb1
이국신 <nw4611@naver.com>
2023-09-25 03:33:05 +0900

    feat: Report의 영역을 구분하는 컴포넌트 구현

---
+++ frontend/src/app/scan/ReportSection.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ReportSection({ children }) {
+  return <div className="w-3/4 p-8 bg-white rounded-small">{children}</div>;
+}
+
+export default ReportSection;
