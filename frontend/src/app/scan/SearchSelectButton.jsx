b2e532338cbe72be96af7e9b282c17c716cfbca6
이국신 <nw4611@naver.com>
2023-09-25 12:32:40 +0900

    feat: 검색 타입 설정 기능 구현

--- frontend/src/app/scan/SearchSelectButton.jsx
+++ frontend/src/app/scan/SearchSelectButton.jsx
@@ -2,7 +2,7 @@ import useHover from '@/hooks/useHover';
 import classnames from 'classnames';
 import React from 'react';
 
-function SearchSelectButton({ text, Icon }) {
+function SearchSelectButton({ text, Icon, onClick }) {
   const [ref, hover] = useHover();
 
   return (
@@ -11,6 +11,7 @@ function SearchSelectButton({ text, Icon }) {
         'w-[150px] h-[150px] border-4 flex flex-col justify-center items-center text-2xl border-disabled text-disabled hover:border-primary hover:text-primary',
       )}
       ref={ref}
+      onClick={onClick}
     >
       <Icon className={classnames('mb-2 fill-disabled', hover ? 'fill-primary' : '')} />
       {text}
