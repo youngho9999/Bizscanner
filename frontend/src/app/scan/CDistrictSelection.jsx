4b78c8ac41bdba85974e559730c5bd0f834384c4
이국신 <nw4611@naver.com>
2023-09-25 14:08:48 +0900

    feat: 상권이 없는 경우에 대한 대응 추가

--- frontend/src/app/scan/CDistrictSelection.jsx
+++ frontend/src/app/scan/CDistrictSelection.jsx
@@ -22,21 +22,27 @@ function CDistrictSelection() {
 
   return (
     <div>
-      <ControllerTitle title="상권을 선택해주세요" />
-      <div className="h-48 overflow-y-auto">
-        <div className="grid grid-cols-3 gap-1 py-3 justify-items-center">
-          {cDistricts.map(({ careaCode, careaName }) => {
-            return (
-              <button
-                key={careaCode}
-                className="text-ellipsis overflow-hidden whitespace-nowrap w-[130px] h-[50px] rounded-small border-disabled text-disabled border-2 hover:border-primary hover:text-white hover:bg-primary"
-              >
-                {careaName}
-              </button>
-            );
-          })}
-        </div>
-      </div>
+      {cDistricts.length > 0 ? (
+        <>
+          <ControllerTitle title="상권을 선택해주세요" />
+          <div className="h-48 overflow-y-auto">
+            <div className="grid grid-cols-3 gap-1 py-3 justify-items-center">
+              {cDistricts.map(({ careaCode, careaName }) => {
+                return (
+                  <button
+                    key={careaCode}
+                    className="text-ellipsis overflow-hidden whitespace-nowrap w-[130px] h-[50px] rounded-small border-disabled text-disabled border-2 hover:border-primary hover:text-white hover:bg-primary"
+                  >
+                    {careaName}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="mt-8 mb-8 text-2xl font-bold text-center">상권이 존재하지 않아요!</div>
+      )}
     </div>
   );
 }
