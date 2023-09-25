94f968d73c2e6abc5560eaf7cb5bad2296e4c755
이국신 <nw4611@naver.com>
2023-09-25 10:37:43 +0900

    feat: 지역 선택 컨텍스트를 사용할 수 있도록 수정

--- frontend/src/app/scan/Controller.jsx
+++ frontend/src/app/scan/Controller.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Selection from './SearchSelection';
+import PlaceSelection from './PlaceSelection';
+import { SearchProvider } from './SearchContext';
 
 function Controller() {
   return (
-    <div className="absolute z-10 p-5 bg-white left-4 rounded-medium top-1/2 translate-y-[-50%]">
-      <Selection />
-    </div>
+    <SearchProvider>
+      <div className="absolute z-10 p-5 bg-white left-4 rounded-medium top-1/2 translate-y-[-50%]">
+        {/* <Selection /> */}
+        <PlaceSelection />
+      </div>
+    </SearchProvider>
   );
 }
 
