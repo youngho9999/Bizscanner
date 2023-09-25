91e2d982bbc20f2d647e8c5b6d2eda1178dcfcdc
연제정 <94yjj@daum.net>
2023-09-25 09:41:02 +0900

    chore: 업종 선택 (소분류) 스크롤바  수정

--- frontend/src/app/scan/JobSelection.jsx
+++ frontend/src/app/scan/JobSelection.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import jdata from '../../../public/jcategory.json';
 
 function JobSelection({ jcategory }) {
-  const jList = jdata[jcategory];  
+  const jList = jdata[jcategory];
 
   return (
     <div className="flex flex-col">
       <div className="text-2xl text-center py-4">{jcategory} 내 세부 업종을 선택해주세요.</div>
-      <div className='h-48 overflow-scroll'>
+      <div className="h-48 overflow-y-auto">
         <div className="grid grid-cols-3 gap-1 justify-items-center py-3">
-          {jList.map(( job, idx) => {
+          {jList.map((job, idx) => {
             return (
               <button
                 key={idx}
