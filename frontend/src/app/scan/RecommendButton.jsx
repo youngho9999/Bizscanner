e898e4618ba01792ae0bb8c40a42a9893a361a4b
이국신 <nw4611@naver.com>
2023-09-26 10:01:39 +0900

    feat: 추천 버튼 구현

---
+++ frontend/src/app/scan/RecommendButton.jsx
@@ -0,0 +1,8 @@
+import Button from '@/components/Button';
+import React from 'react';
+
+function RecommendButton({ title, onClick }) {
+  return <Button className="p-4 mt-4 text-white rounded-small bg-primary" onClick={onClick}>{title}</Button>;
+}
+
+export default RecommendButton;
