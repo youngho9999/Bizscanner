1ea0da9a20119d482e281a74534d0b8b23a1bb27
이국신 <nw4611@naver.com>
2023-09-26 15:53:26 +0900

    feat: 검색 컨텍스트 적용 위치 수정 레포트에서도 검색 컨텍스트가 사용되어 위치를 변경 시킴

--- frontend/src/app/scan/page.jsx
+++ frontend/src/app/scan/page.jsx
@@ -3,15 +3,18 @@ import data from '../../../public/상권특구.geojson';
 import Map from './Map';
 import Controller from './Controller';
 import Report from './Report';
+import { SearchProvider } from './SearchContext';
 
 function page() {
   return (
     <>
-      <div className="relative flex grow">
-        <Map commerialDistricts={data} />
-        <Controller />
-      </div>
-      <Report />
+      <SearchProvider>
+        <div className="relative flex grow">
+          <Map commerialDistricts={data} />
+          <Controller />
+        </div>
+        <Report />
+      </SearchProvider>
     </>
   );
 }
