764adb525174671c9f7c3b8cf39429728dbdb7c9
구기현 <rnrl4611@gmail.com>
2023-09-26 17:27:31 +0900

    fix: nickname update api 위한 ERRORCODE

--- backend/bizscanner/src/main/java/store/bizscanner/global/exception/ErrorCode.java
+++ backend/bizscanner/src/main/java/store/bizscanner/global/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     RECOMMEND_JCATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "추천할 업종이 없습니다"),
     RECOMMEND_CAREA_NOT_FOUND(HttpStatus.NOT_FOUND, "추천할 상권이 없습니다"),
-    SCRAP_NOT_FOUND(HttpStatus.NOT_FOUND, "Scrap Resource not exists");
+    SCRAP_NOT_FOUND(HttpStatus.NOT_FOUND, "Scrap Resource not exists"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "member not found");
 
     private final HttpStatus httpStatus;
     private final String message;
