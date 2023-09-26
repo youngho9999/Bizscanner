29b650d29d59223d3075ad2d3ee30c0967e1f189
연제정 <94yjj@daum.net>
2023-09-26 17:09:53 +0900

    chore: 모달 공통 컴포넌트 z-index 변경

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import ReactDOM from 'react-dom';
 
 function ModalMain({ children, isOpen }) {
+  console.log(isOpen);
   if (!isOpen) {
     return null;
   }
@@ -11,7 +12,7 @@ function ModalMain({ children, isOpen }) {
 
 function ModalDimmed({ children, onClick }) {
   return (
-    <div className="fixed left-0 top-0 w-full h-[100vh] bg-dim" onClick={onClick}>
+    <div className="fixed left-0 top-0 w-full h-[100vh] bg-dim z-50" onClick={onClick}>
       {children}
     </div>
   );
@@ -21,7 +22,7 @@ function ModalContainer({ children, className, width, height }) {
   return (
     <div
       className={classnames(
-        `p-10 fixed top-1/2 left-1/2 w-1/2 translate-x-[-50%] translate-y-[-50%] rounded-large`,
+        `p-10 fixed top-1/2 left-1/2 w-1/2 translate-x-[-50%] translate-y-[-50%] rounded-large z-50`,
         className,
       )}
       style={{
