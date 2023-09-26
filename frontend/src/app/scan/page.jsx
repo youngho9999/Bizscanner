fef4c144ac88b0f7040928e4e53844b929bbd72a
이국신 <nw4611@naver.com>
2023-09-26 13:21:59 +0900

    feat: 레포트 페이지 추가

--- frontend/src/app/scan/page.jsx
+++ frontend/src/app/scan/page.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import data from '../../../public/상권특구.geojson';
 import Map from './Map';
 import Controller from './Controller';
+import Report from './Report';
 
 function page() {
   return (
-    <div className="relative flex grow">
-      <Map commerialDistricts={data} />
-      <Controller />
-    </div>
+    <>
+      <div className="relative flex grow">
+        <Map commerialDistricts={data} />
+        <Controller />
+      </div>
+      <Report />
+    </>
   );
 }
 
