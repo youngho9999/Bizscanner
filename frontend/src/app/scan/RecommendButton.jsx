47f7b3c73658bad8c399431fbbb658f05d1a32a9
이국신 <nw4611@naver.com>
2023-09-26 10:14:38 +0900

    design: 추천 버튼이 스타일 수정

--- frontend/src/app/scan/RecommendButton.jsx
+++ frontend/src/app/scan/RecommendButton.jsx
@@ -2,7 +2,11 @@ import Button from '@/components/Button';
 import React from 'react';
 
 function RecommendButton({ title, onClick }) {
-  return <Button className="p-4 mt-4 text-white rounded-small bg-primary" onClick={onClick}>{title}</Button>;
+  return (
+    <Button className="w-full p-4 mt-4 text-white rounded-small bg-primary" onClick={onClick}>
+      {title}
+    </Button>
+  );
 }
 
 export default RecommendButton;
