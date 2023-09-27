d5aba63212ba2970b93f8457b720f76904e5cf68
이국신 <nw4611@naver.com>
2023-09-27 13:45:54 +0900

    design: 요약 텍스트 밑에 마진 추가

--- frontend/src/app/scan/SummaryText.jsx
+++ frontend/src/app/scan/SummaryText.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function SummaryText({ children }) {
-  return <div className="py-4 text-center px-7 bg-background">{children}</div>;
+  return <div className="py-4 mb-8 text-center px-7 bg-background">{children}</div>;
 }
 
 export default SummaryText;
