53dc3a4891ad9a4f1f1bccac44d3a98920cc1aac
구기현 <rnrl4611@gmail.com>
2023-09-27 14:21:46 +0900

    fix: cors 설정

--- backend/bizscanner/src/main/java/store/bizscanner/global/config/security/SecurityConfig.java
+++ backend/bizscanner/src/main/java/store/bizscanner/global/config/security/SecurityConfig.java
@@ -65,10 +65,7 @@ public class SecurityConfig {
                 //== URL별 권한 관리 옵션 ==//
                 .authorizeRequests()
 
-                // 아이콘, css, js 관련
-                // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                 .antMatchers("/**").permitAll() // 임시 전체 허용
-                .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
                 .antMatchers("/member/**", "ouath2/**", "/sign-up", "/report/**", "/carea-recommend", "/jcategory-recommend/**").permitAll() // 회원가입 접근 가능
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
@@ -83,6 +80,9 @@ public class SecurityConfig {
         // 순서 : LogoutFilter -> JwtAuthenticationProcessingFilter -> CustomJsonUsernamePasswordAuthenticationFilter
         http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
         http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
+
+        // cors 설정
+        http.cors(); 
 
         return http.build();
     }
