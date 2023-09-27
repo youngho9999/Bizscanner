25ee826dc07963af7678bfa2d1820c4d30e1c5c2
구기현 <rnrl4611@gmail.com>
2023-09-27 09:01:37 +0900

    fix: 정규식 수정 및 테스트 완료

--- backend/bizscanner/src/main/java/store/bizscanner/dto/request/UpdateNicknameRequest.java
+++ backend/bizscanner/src/main/java/store/bizscanner/dto/request/UpdateNicknameRequest.java
@@ -13,7 +13,7 @@ public class UpdateNicknameRequest {
     @Email
     private String email;
 
-    @Pattern(regexp = "^[^!@#$%^&*()_+|<>?:{}ㄱ-ㅎㅏ-ㅣ가-힣]*[a-zA-Z]{2,16}[^!@#$%^&*()_+|<>?:{}ㄱ-ㅎㅏ-ㅣ가-힣]*$", message = "2 ~ 16 글자 이내, 특수문자, 자음 , 모음 제외")
+    @Pattern(regexp = "^[가-힣a-zA-Z]{2,16}$", message = "2 ~ 16 글자 이내, 특수문자, 자음 , 모음 제외")
     private String nickname;
 
 
