6927eee49a29611d88fb1438ae27cf3d55988ecf
이국신 <nw4611@naver.com>
2023-09-27 12:12:57 +0900

    feat: 그래프 상수 정의

---
+++ frontend/src/components/Graph/constants.js
@@ -0,0 +1,26 @@
+export const timeLabel = [
+  '0시~ 4시',
+  '5시 ~ 8시',
+  '9시 ~ 12시',
+  '13시 ~ 16시',
+  '17시 ~ 21시',
+  '22시~ 24시',
+];
+
+export const genderConfig = {
+  labels: ['남성', '여성'],
+  backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+  borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)'],
+};
+
+export const quaterConfig = {
+  labels: ['2022년 1분기', '2022년 2분기', '2022년 3분기', '2022년 4분기', '2023년 1분기'],
+};
+
+export const dayConfig = {
+  labels: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+};
+
+export const ageConfig = {
+  labels: ['10대', '20대', '30대', '40대', '50대', '60대'],
+};
