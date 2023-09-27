41c3e76802ea5686b184d2a58a651e94c09969d4
이국신 <nw4611@naver.com>
2023-09-27 12:14:09 +0900

    chore: 린팅 수정

--- frontend/src/app/scan/ReportStoreCount.jsx
+++ frontend/src/app/scan/ReportStoreCount.jsx
@@ -4,7 +4,13 @@ import SummaryText from './SummaryText';
 import { useSearchState } from './SearchContext';
 import axios from '@/api/index';
 import Bar from '@/components/Graph/BarGraph';
-import { getLastYearDiff, getLastYearDiffText, getPrevQuaterDiff, getPrevQuaterDiffText } from '@/utils/diff';
+import {
+  getLastYearDiff,
+  getLastYearDiffText,
+  getPrevQuaterDiff,
+  getPrevQuaterDiffText,
+} from '@/utils/diff';
+import { quaterConfig } from '@/components/Graph/constants';
 
 function ReportStoreCount() {
   const [storeCountInfo, setStoreCountInfo] = useState();
@@ -23,12 +29,16 @@ function ReportStoreCount() {
     <ReportSection title="점포수">
       <SummaryText>
         {storeCountInfo &&
-          `해당 상권에서 운영 중인 한식 점포수 전년 동분기 대비 ${getLastYearDiff(storeCountInfo)}개 ${getLastYearDiffText(storeCountInfo)}
-      하였으며,전분기 대비 ${getPrevQuaterDiff(storeCountInfo)}개 ${getPrevQuaterDiffText(storeCountInfo)}하였습니다.`}
+          `해당 상권에서 운영 중인 한식 점포수 전년 동분기 대비 ${getLastYearDiff(
+            storeCountInfo,
+          )}개 ${getLastYearDiffText(storeCountInfo)}
+      하였으며,전분기 대비 ${getPrevQuaterDiff(storeCountInfo)}개 ${getPrevQuaterDiffText(
+            storeCountInfo,
+          )}하였습니다.`}
       </SummaryText>
       <div className="flex items-center justify-center">
         <div className="w-3/4">
-          <Bar graphData={storeCountInfo} title="점포수" />
+          <Bar graphData={storeCountInfo} title="점포수" config={quaterConfig} />
         </div>
       </div>
     </ReportSection>
