724fd49826255598ef2a2d54c74b16059ad213da
이국신 <nw4611@naver.com>
2023-10-04 02:27:50 +0900

    design: 레포트 영역 타이틀 마진 바텀 조정

--- frontend/src/app/scan/ReportSection.jsx
+++ frontend/src/app/scan/ReportSection.jsx
@@ -4,7 +4,7 @@ import React, { forwardRef } from 'react';
 const ReportSection = forwardRef(function ReportSection({ children, title, className }, ref) {
   return (
     <div className={classnames('p-8 bg-white rounded-small', className)} ref={ref}>
-      {title && <div className="mb-8 text-2xl font-bold">{title}</div>}
+      {title && <div className="mb-4 text-2xl font-bold">{title}</div>}
       {children}
     </div>
   );
