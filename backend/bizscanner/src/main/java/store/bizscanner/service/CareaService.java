c47b8d024bb5d7184a8fda9bbc07ee1dd603ff6b
전영호 <aplblypt@gmail.com>
2023-10-04 20:21:43 +0900

    fix: 안쓰는 import 문 제거

--- backend/bizscanner/src/main/java/store/bizscanner/service/CareaService.java
+++ backend/bizscanner/src/main/java/store/bizscanner/service/CareaService.java
@@ -14,7 +14,6 @@ import store.bizscanner.repository.CareaRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
