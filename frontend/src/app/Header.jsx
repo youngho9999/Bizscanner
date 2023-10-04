da59a42589cd3554756403198c071d5af8e9a955
이국신 <nw4611@naver.com>
2023-10-04 14:47:48 +0900

    feat: 로그인 여부에 따른 헤더 UI 변경

--- frontend/src/app/Header.jsx
+++ frontend/src/app/Header.jsx
@@ -1,15 +1,18 @@
+'use client';
 import LoginButton from '@/components/User/LoginButton';
 import Profile from '@/components/User/Profile';
 import Link from 'next/link';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const isLogin = useSelector((state) => state.user.isLogin);
   return (
-    <nav className="bg-background w-full p-4 flex justify-between items-center">
+    <nav className="flex items-center justify-between w-full p-4 bg-background">
       <Link href="/">
         <img src="icons/logo.svg" />
       </Link>
-      <Profile />
+      {isLogin ? <Profile /> : <LoginButton />}
     </nav>
   );
 }
