9864009832a76a950d3736628a33662111bff072
전영호 <aplblypt@gmail.com>
2023-10-04 19:35:14 +0900

    feat: 업종 추천 - 영역 폴리곤 API 의 area 의 서비스 로직, response dto 생성

---
+++ backend/bizscanner/src/main/java/store/bizscanner/service/CodeCoordinateService.java
@@ -0,0 +1,32 @@
+package store.bizscanner.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import store.bizscanner.dto.response.jcategoryrecommend.CodeAreaPolygonAndCenterResponse;
+import store.bizscanner.entity.CodeCenterCoordinate;
+import store.bizscanner.entity.CodeCoordinate;
+import store.bizscanner.global.exception.CustomException;
+import store.bizscanner.global.exception.ErrorCode;
+import store.bizscanner.repository.CodeCenterCoordinateRepository;
+import store.bizscanner.repository.CodeCoordinateRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CodeCoordinateService {
+
+    private final CodeCenterCoordinateRepository codeCenterCoordinateRepository;
+    private final CodeCoordinateRepository codeCoordinateRepository;
+
+    public CodeAreaPolygonAndCenterResponse getCodeAreaPolygonAndCenter(String code) {
+        //해당 영역 중심 위도,경도
+        CodeCenterCoordinate center = codeCenterCoordinateRepository.findById(code)
+                .orElseThrow(() -> new CustomException(ErrorCode.AREA_NOT_FOUND));
+
+        //해당 영역 polygon 그리기 위한 좌표들
+        List<CodeCoordinate> polygon = codeCoordinateRepository.findByCode(code);
+
+        return new CodeAreaPolygonAndCenterResponse(center, polygon);
+    }
+}
