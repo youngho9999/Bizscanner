9bcefeae783c0d0d9e0d774958b8dafab1e5a58a
이국신 <nw4611@naver.com>
2023-10-04 15:59:06 +0900

    feat: 댓글 등록 기능 구현

--- frontend/src/app/scan/ReportComment.jsx
+++ frontend/src/app/scan/ReportComment.jsx
@@ -1,23 +1,43 @@
-import React, { forwardRef, useEffect, useState } from 'react';
+import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import ReportSection from './ReportSection';
 import SendIcon from '@/assets/icons/Send.svg';
 import axios from '@/api/index';
 import { useSearchState } from './SearchContext';
 
-function ReportCommentItem({ nickName, content }) {
+function ReportCommentItem({ nickName, contents }) {
   return (
     <div className="py-4 border-b border-black">
       <div className="text-xl font-bold">{nickName}</div>
-      <div>{content}</div>
+      <div>{contents}</div>
     </div>
   );
 }
 
-const CommentInput = forwardRef(function CommentInput({ placeholder }, ref) {
+const CommentInput = forwardRef(function CommentInput({ placeholder, fetchComments }, ref) {
+  const { careaCode, jcategoryCode } = useSearchState();
+
+  const inputRef = useRef(null);
+  const onClickSendComment = async () => {
+    const value = inputRef.current.value;
+
+    await axios.post(`/comment`, {
+      careaCode,
+      jcategoryCode,
+      contents: value,
+    });
+
+    fetchComments();
+
+    inputRef.current.value = '';
+  };
+
   return (
     <div className="flex items-center h-12 px-4 overflow-hidden border rounded-full border-outline">
-      <input placeholder={placeholder} className="h-full grow outline-0" />
-      <button className="relative flex items-center w-8 h-8 p-1 ml-1 rounded-full bg-primary">
+      <input placeholder={placeholder} className="h-full grow outline-0" ref={inputRef} />
+      <button
+        className="relative flex items-center w-8 h-8 p-1 ml-1 rounded-full bg-primary"
+        onClick={onClickSendComment}
+      >
         <SendIcon width="22" className="absolute right-[6px]" />
       </button>
     </div>
@@ -29,33 +49,24 @@ function ReportComment() {
   const { careaCode } = useSearchState();
 
   const fetchComments = async () => {
-    const { data } = await axios.get(`/comment/${careaCode}`);
-    setComments(data);
+    const {
+      data: { commentResponseList },
+    } = await axios.get(`/comment/${careaCode}`);
+    setComments(commentResponseList);
   };
 
   useEffect(() => {
-    // fetchComments();
+    fetchComments();
   }, []);
   return (
     <ReportSection title="코멘트" className="h-full">
       <div className="overflow-auto h-[80%] scrollbar-hide mb-4">
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
-        <ReportCommentItem nickName="닉네임 1" content="상권 정말 좋아요!" />
+        {comments.length &&
+          comments.map(({ nickname, contents }) => (
+            <ReportCommentItem nickName={nickname} contents={contents} />
+          ))}
       </div>
-      <CommentInput placeholder="댓글 추가" />
+      <CommentInput placeholder="댓글 추가" fetchComments={fetchComments} />
     </ReportSection>
   );
 }
