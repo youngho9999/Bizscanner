f3a7b66689a40363e14f59f594191d70eddc1927
이국신 <nw4611@naver.com>
2023-10-05 22:47:13 +0900

    feat: 사용자 메뉴 외부를 클릭하면 메뉴가 닫히도록 수정

--- frontend/src/components/User/ProfileMenu.jsx
+++ frontend/src/components/User/ProfileMenu.jsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom';
 import { logout } from '@/redux/userSlice';
 import { convertNickName } from '@/utils/nickname';
 import Link from 'next/link';
@@ -20,44 +21,54 @@ function ProfileMenu({ onClickEditNickname, onCloseMenu }) {
     onCloseMenu();
   };
 
+  const onClickBackGround = () => {
+    onCloseMenu();
+  };
+
   return (
-    <div className="absolute py-4 top-16 right-7 bg-background w-[370px] rounded-small shadow-lg z-50">
-      <div className="mb-4 text-center">{email}</div>
-      <div className="flex items-center justify-center mb-4">
-        <div
-          className="flex items-center justify-center w-16 h-16 text-xl font-bold text-white rounded-full"
-          style={{
-            background: '#8D6E63',
-          }}
-        >
-          <span
-            className="text-3xl"
+    <>
+      <div className="absolute py-4 top-16 right-7 bg-background w-[370px] rounded-small shadow-lg z-50">
+        <div className="mb-4 text-center">{email}</div>
+        <div className="flex items-center justify-center mb-4">
+          <div
+            className="flex items-center justify-center w-16 h-16 text-xl font-bold text-white rounded-full"
             style={{
-              position: 'relative',
-              bottom: '0.2em',
+              background: '#8D6E63',
             }}
           >
-            {convertNickName(nickname)}
-          </span>
+            <span
+              className="text-3xl"
+              style={{
+                position: 'relative',
+                bottom: '0.2em',
+              }}
+            >
+              {convertNickName(nickname)}
+            </span>
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="text-lg text-center">안녕하세요. {nickname}님</div>
+        </div>
+        <div>
+          <button className="w-full p-2 hover:bg-outline" onClick={onClickEdit}>
+            닉네임 변경
+          </button>
+          <button className="w-full p-2 hover:bg-outline" onClick={onClickMyReport}>
+            <Link href="/myreport" className="block">
+              마이 리포트
+            </Link>
+          </button>
+          <button className="w-full p-2 hover:bg-outline" onClick={onClickLogout}>
+            로그아웃
+          </button>
         </div>
       </div>
-      <div className="mb-4">
-        <div className="text-lg text-center">안녕하세요. {nickname}님</div>
-      </div>
-      <div>
-        <button className="w-full p-2 hover:bg-outline" onClick={onClickEdit}>
-          닉네임 변경
-        </button>
-        <button className="w-full p-2 hover:bg-outline" onClick={onClickMyReport}>
-          <Link href="/myreport" className="block">
-            마이 리포트
-          </Link>
-        </button>
-        <button className="w-full p-2 hover:bg-outline" onClick={onClickLogout}>
-          로그아웃
-        </button>
-      </div>
-    </div>
+      {createPortal(
+        <div className="fixed z-40 w-full h-full bg-green" onClick={onClickBackGround}></div>,
+        document.body,
+      )}
+    </>
   );
 }
 
