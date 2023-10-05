1ae53075261c18502074f8f3c93dd2c655243082
이국신 <nw4611@naver.com>
2023-10-05 17:14:16 +0900

    feat: 로티를 활용한 랜딩 페이지 구현

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,34 @@
+'use client';
 import Button from '@/components/Button';
+import Lottie from 'lottie-react';
 import Link from 'next/link';
+import LandingLottie from '@/assets/lotties/Landing.json';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+  const lottieRef = useRef(null);
+
+  useEffect(() => {
+    lottieRef.current.setSpeed(0.7);
+  }, []);
+
+  const lottieStyle = {
+    position: 'fixed',
+    zIndex: -1,
+    flexGrow: '1',
+    width: '100%',
+  };
   return (
-    <main className="flex items-center justify-center grow">
+    <main className="flex flex-col items-center justify-center grow">
+      <Lottie
+        autoPlay={true}
+        animationData={LandingLottie}
+        loop={true}
+        style={lottieStyle}
+        lottieRef={lottieRef}
+      />
       <div className="text-4xl font-bold text-center">
-        <p className="mb-8">
+        <p className="mb-8 text-5xl drop-shadow-2xl">
           창업의 모든것 <br />
           비즈 스캐너에서 쉽고 간편하게
         </p>
