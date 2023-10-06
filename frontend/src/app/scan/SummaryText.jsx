13a949eb09bdd2929ba890c0605135700547ac6d
이국신 <nw4611@naver.com>
2023-10-06 09:45:10 +0900

    design: 요약 텍스트 중앙 정렬

--- frontend/src/app/scan/SummaryText.jsx
+++ frontend/src/app/scan/SummaryText.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function SummaryText({ children }) {
-  return <div className="py-4 mb-8 text-center px-7 bg-background max-w-[800px]">{children}</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="py-4 mb-8 text-center px-7 bg-background max-w-[800px]">{children}</div>
+    </div>
+  );
 }
 
 export default SummaryText;
